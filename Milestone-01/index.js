03330766fb9d35894ed633e2efe1dbffdbbc34d0
Syed Konain Nasir <konain611@gmail.com>
2024-09-15 19:31:04 +0500

    Added Show/Hide Buttons

---
+++ Milestone-01/index.js
@@ -0,0 +1,56 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunction1() {
+  var x = document.getElementById("myDIV1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunction2() {
+  var y = document.getElementById("myDIV2");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
+function myFunction3() {
+  var y = document.getElementById("myDIV3");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
+function myFunction4() {
+  var y = document.getElementById("myDIV4");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
+function myFunction5() {
+  var y = document.getElementById("myDIV5");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
+function myFunction6() {
+  var y = document.getElementById("myDIV6");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
